5b6092c57596b4ebf5337ca8231e6e1683b6f6a5
Vamshi Krishna <vamshiyadav4701@gmail.com>
2025-02-15 15:18:46 +0530

    new portfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -89,7 +89,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/vamshikrishna___v"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
